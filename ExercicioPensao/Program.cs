337e14b4e6c2394624a80cbdff6408b9f2299fc5
Ricardo <ricardo.senai.go@gmail.com>
2021-04-22 12:01:11 -0300

    iniciado

---
+++ ExercicioPensao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Quarto.Calcula(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ ExercicioPensao/Quarto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPensao
+{
+    class Quarto
+    {
+      
+
+        public static void Calcula (ref int x){
+            x = x * 3;
+            
+            }
+    }
+}
